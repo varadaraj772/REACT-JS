c2d407b6c813380d6b9d58c4fbd8d738c9c205ce
varadaraj772 <varadaraj277@gmail.com>
2024-01-26 18:50:50 +0530

    some changes

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,14 +1,60 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { Inputs } from "./components";
+import useCurrencyInfo from "./custom_hooks/use_info";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [converted_amnt, setConvertedAmnt] = useState(0);
+  const currency_info = useCurrencyInfo(from);
+  const options = Object.keys(currency_info);
+  console.log(options);
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmnt(amount);
+    setAmount(converted_amnt);
+  };
+  const convert = () => {
+    setConvertedAmnt(amount * currency_info[to]);
+  };
 
   return (
     <>
-     <h1 className=' text-center'>Currency Converter</h1>
+      <div className=" bg-slate-300 text-center ">
+        <h1 className=" text-center">Currency Converter</h1>
+
+        <Inputs
+          label="from"
+          amount={amount}
+          currencyOpt={options}
+          onCurrencyChange={(currency) => setFrom(currency)}
+          selectCurrency={from}
+          onAmountChange={(amount) => setAmount(amount)}
+        ></Inputs>
+        <button
+          className=" bg-blue-400 outline-none rounded-lg p-4"
+          onClick={() => swap()}
+        >
+          SWAP
+        </button>
+        <Inputs
+          label="to"
+          amount={converted_amnt}
+          currencyOpt={options}
+          onCurrencyChange={(currency) => setTo(currency)}
+          selectCurrency={to}
+        ></Inputs>
+        <button
+          className=" bg-blue-400 outline-none rounded-lg p-4"
+          onClick={() => convert()}
+        >
+          CONVERT from {from} to {to}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ currency_converter/src/components/Inputs.jsx
@@ -0,0 +1,55 @@
+import { useId } from "react";
+
+function Inputs({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOpt = [],
+  selectCurrency = "usd",
+}) {
+  const amntId = useId();
+  return (
+    <div>
+      <div className={` p-3 rounded-md text-sm flex`}>
+        <div className=" w-1/2 bg-slate-200 rounded-lg">
+          <label
+            className=" text-black/40 mb-2 inline-block mx-3"
+            htmlFor={amntId}
+          >
+            {label}
+          </label>
+          <input
+            id={amntId}
+            className=" rounded-lg bg-slate-400 py-1.5 px-7"
+            type="number"
+            placeholder="Amount"
+            value={amount}
+            onChange={(e) =>
+              onAmountChange && onAmountChange(Number(e.target.value))
+            }
+          ></input>
+        </div>
+        <div className=" w1/2 flex flex-wrap justify-end text-right">
+          <p className=" text-black/40 mb-2 w-full">Currency Type</p>
+          <select
+            className=" rounded-lg p-1 bg-gray-100 cursor-pointer outline-none"
+            value={selectCurrency}
+            onChange={(e) =>
+              onCurrencyChange && onCurrencyChange(e.target.value)
+            }
+          >
+            {currencyOpt.map((Currency) => (
+        
+              <option key={Currency} value={Currency}>
+                {Currency}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Inputs;
